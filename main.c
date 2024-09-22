d6a6f8fc773db0be7b8d83d015a9cc64b0579687
João235 <joao39714@gmail.com>
2024-09-21 21:50:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main() {
     printf("Digite o numero de casos: ");
     scanf("%d", &numerocasos);
     
-    for(j = 0; j < numerocasos; j++) {
+    for(j = 0; j < numerocasos; j++) {   //O programa entra em um loop que se repete 'numerocasos' vezes. Cada iteração do loop processa uma mensagem diferente.
         printf("Digite o valor de b: ");
         scanf("%d", &b);
 
@@ -22,18 +22,18 @@ int main() {
         
         printf("\n");
     
-        for(i = 0; i < strlen(mensagem); i += 2) {
-            x = i / 2 + 1;
+        for(i = 0; i < strlen(mensagem); i += 2) {  //Repete pela string em passos de 2 caracteres (ou seja, processa cada par de caracteres).
+            x = i / 2 + 1;   // Calcula o valor de 'x'(posição atual do caractere) com base no índice de repetição atual 'i'.
             verificacao = func_val(x, b);
-            if(verificacao != 0) {
+            if(verificacao != 0) {  //Se verificacao for diferente de zero, ela extrai o par de caracteres atual da mensagem e armazena na string hexadecimal.
                 hexadecimal[0] = mensagem[i];
                 hexadecimal[1] = mensagem[i + 1];
                 hexadecimal[2] = '\0';
-                if(strcmp(hexadecimal, fim) == 0) {
+                if(strcmp(hexadecimal, fim) == 0) {  //Verifica se o par extraído é igual à "00", se for, o loop é terminado.
             	    break;
 		}
-            resultado = strtol(hex, NULL, 16);
-            printf("%c", resultado);
+            resultado = strtol(hex, NULL, 16);  //Converte o par extraído de hexadecimal para um inteiro.
+            printf("%c", resultado);  //Imprime o caractere correspondente ao inteiro convertido na tabela ASCII.
             }
         }
         printf("\n\n\n");
