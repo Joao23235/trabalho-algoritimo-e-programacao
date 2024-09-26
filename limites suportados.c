dce557663462a3d7b2128c7dc9066b4f781c738f
João235 <joao39714@gmail.com>
2024-09-25 21:20:30 -0300

    Update limites suportados.c

--- limites suportados.c
+++ limites suportados.c
@@ -5,16 +5,23 @@ int main() {
 	
 	printf("Limite minimo char: %d\n", CHAR_MIN);
 	printf("Limite maximo char: %d\n", CHAR_MAX);
+
 	printf("Limite minimo int: %d\n", INT_MIN);
 	printf("Limite maximo int: %d\n", INT_MAX);
+
 	printf("Limite minimo short int: %d\n", SHRT_MIN);
 	printf("Limite maximo short int: %d\n", SHRT_MAX);
+
 	printf("Limite maximo unsigned int: %u\n", UINT_MAX);
+
 	printf("Limite minimo long int: %ld\n", LONG_MIN);
 	printf("Limite minimo long int: %ld\n", LONG_MAX);
+
 	printf("Limite maximo unsigned long int: %lu\n", ULONG_MAX);
+
 	printf("Limite minimo long long int: %lld\n", LLONG_MIN);
 	printf("Limite maximo long long int: %lld\n", LLONG_MAX);
+
 	printf("Limite maximo unsigned long long int: %llu\n", ULLONG_MAX);
 	
 	printf("\n");
